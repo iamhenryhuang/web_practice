97f8071cbd5dfe5c34cafde5a8b1a92576004c88
iamhenryhuang <henry.curry1008@gmail.com>
2024-02-21 18:43:03 +0800

    update

---
+++ clock/script.js
@@ -0,0 +1,23 @@
+// 抓秒針 分針 時針
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+// 建立函式
+function setClock() {
+    var currentDate = new Date();
+    var secondsRatio = currentDate.getSeconds() / 60;
+    var minuteRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    var hoursRatio = (minuteRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minuteRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setInterval(setClock, 1000);
+
+setClock();
